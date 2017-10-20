43b9be9baa891a27a46fe660ac995d777144087b
Pauline Dame <paulinedame3@gmail.com>
2017-10-20 13:42:13 +0200

    receiving information from PHP to display the correct trial

--- main.py
+++ main.py
@@ -5,50 +5,19 @@ Created on Thu Sep 28 17:32:23 2017
 @author: Pauline Dame
 """
 
-import keyboard
-import time 
 import random as rd
 import matplotlib.pyplot as plt
 import csv
 import os
 from math import *
 import mpld3 
-import json
 
 # --------- GLOBAL PARAMETERS -----------------------------------------------------
-
-
-#Parameters of the figure (window and dots)
-size_windows = 10
-radius = float(size_windows)/50
-dist = 1
-size = radius + size_windows
 
 #Parameters of the CSV file
 save_path_csv ="D:\AI Master\Project MAIR\Test"
 
-#Get these input using PHP
-#name = raw_input('What is your name ? ')
-#age = input('What is your age ?  ')
-#gender = raw_input('What is your gender ? (m/f) ')
-#colour_bld = raw_input('Are you colour blind ? (y/n) ')
-#eyesight = raw_input('Do you have a corrected or normal eyesight ? (c/n) ')
-#native_language = raw_input('What is your native language (english, dutch, french, spanish, german) ?) ')    
 
-#Creating the list of all the 16 types of trials
-ratios = [(4,5),(3,5),(2,5),(1,5),(29,71),(17,83),(10,90),(2,98)]
-experiments = []
-for i in range(2) : 
-    for j in range(len(ratios)):
-        experiments.append((ratios[j],i))
-#Creating a list to keep track of all the 80 trials which should be done during on experiment
-index = []
-for j in range(5):
-    for i in range(16):
-        index.append(i)
-        
-exp = ((4,5),0,0)
-        
 
 #----------FUNCTIONS -----------------------------------------------------------------
 
@@ -77,87 +46,87 @@ def get_dots_coordinates(n1,n2,dist) :
         while check < dist_min : 
             x ,y = rd.uniform(-size+radius,size-radius) , rd.uniform(-size+radius,size-radius)
             check = check_distance_dots(x,y,list_dots,dist_min)
-        string_dots_blue += '{x:'+str(x)+', y: '+str(y)+', z:0.5, radius :0.2,color : "Blue" },'
+        string_dots_blue += '{x:'+str(x)+', y: '+str(y)+', z:3,color : "Blue" },'
         list_dots.append((x,y))
     for yellow_dot in range(n2):
         check = 0
         while check < dist_min : 
             x ,y = rd.uniform(-size+radius,size-radius) , rd.uniform(-size+radius,size-radius)
             check = check_distance_dots(x,y,list_dots,dist_min)
-        string_dots_yellow += '{x:'+str(x)+', y: '+str(y)+', z:0.5,radius : 0.2,color : "Yellow"},'
+        string_dots_yellow += '{x:'+str(x)+', y: '+str(y)+', z:3,color : "Yellow"},'
         list_dots.append((x,y))
     return string_dots_blue, string_dots_yellow
 
-def display_image(size,n1,n2,radius,dist,legend) : 
-                    
-    fig, ax = plt.subplots()
-    dist_min = dist   
-    list_dots = []
-    for red_dot in range(n1):
-        check = 0
-        while check < dist_min : 
-            x ,y = rd.uniform(-size+radius,size-radius) , rd.uniform(-size+radius,size-radius)
-            check = check_distance_dots(x,y,list_dots,dist_min)
-        list_dots.append((x,y))            
-        ax.add_artist(plt.Circle((x,y),radius,color='y'))
-    for red_dot in range(n2):
-        check = 0
-        while check < dist_min : 
-            x ,y = rd.uniform(-size+radius,size-radius) , rd.uniform(-size+radius,size-radius)
-            check = check_distance_dots(x,y,list_dots,dist_min)
-        list_dots.append((x,y))                        
-        ax.add_artist(plt.Circle((x,y),radius,color='b'))
-            
-        
-    plt.xlabel(legend)
-    ax.set_xlim((-size, size))
-    ax.set_ylim((-size, size))
-    plt.ion()
-    plt.show()
-    plt.pause(.001)
-    return mpld3.fig_to_html(fig)
-
-            
-def display_image_paired_dots(size,n1,n2,radius,dist,legend) : 
-
-    fig, ax = plt.subplots()
-    dist_min = dist   
-    list_dots = []   
-    nbr_pairs = min(n1,n2)
-    for pair in range(nbr_pairs) :
-        check = 0
-        while check < dist_min : 
-            x1 ,y1 = rd.uniform(-size+dist,size-dist) , rd.uniform(-size+dist,size-dist) 
-            check = check_distance_dots(x1,y1,list_dots,dist_min)
-        list_dots.append((x1,y1))
-        check = 0
-        while check < dist_min : 
-            dx , dy = rd.choice([rd.uniform(-300*radius,-dist*100), rd.uniform(300*radius,dist*100)])/100 , rd.choice([rd.uniform(-300*radius,-dist*50),rd.uniform(300*radius,dist*50)])/100
-            x2 , y2 = x1 + dx , y1 + dy
-            check = check_distance_dots(x2,y2,list_dots,dist_min)
-        list_dots.append((x2,y2))
-        ax.add_artist(plt.Circle((x1,y1),radius,color='b'))
-        ax.add_artist(plt.Circle((x2,y2),radius,color='y'))
-    nbr_isolated_dots = abs(n1-n2)
-    color_dots = 'b'
-    if n1 > n2 : 
-        color_dots = 'y'
-    for dot in range(nbr_isolated_dots) : 
-        check = 0
-        while check < dist_min : 
-            x ,y = rd.uniform(-size+dist,size-dist) , rd.uniform(-size+dist,size-dist)
-            check = check_distance_dots(x,y,list_dots,dist_min)
-        ax.add_artist(plt.Circle((x,y),radius,color=color_dots))
-    return mpld3.fig_to_html(fig)
-            
-    
-    plt.xlabel(legend)
-    ax.set_xlim((-size, size))
-    ax.set_ylim((-size, size))
-    plt.ion()
+#def display_image(size,n1,n2,radius,dist,legend) : 
+#                    
+#    fig, ax = plt.subplots()
+#    dist_min = dist   
+#    list_dots = []
+#    for red_dot in range(n1):
+#        check = 0
+#        while check < dist_min : 
+#            x ,y = rd.uniform(-size+radius,size-radius) , rd.uniform(-size+radius,size-radius)
+#            check = check_distance_dots(x,y,list_dots,dist_min)
+#        list_dots.append((x,y))            
+#        ax.add_artist(plt.Circle((x,y),radius,color='y'))
+#    for red_dot in range(n2):
+#        check = 0
+#        while check < dist_min : 
+#            x ,y = rd.uniform(-size+radius,size-radius) , rd.uniform(-size+radius,size-radius)
+#            check = check_distance_dots(x,y,list_dots,dist_min)
+#        list_dots.append((x,y))                        
+#        ax.add_artist(plt.Circle((x,y),radius,color='b'))
+#            
+#        
+#    plt.xlabel(legend)
+#    ax.set_xlim((-size, size))
+#    ax.set_ylim((-size, size))
+#    plt.ion()
 #    plt.show()
 #    plt.pause(.001)
-    return fig 
+#    return mpld3.fig_to_html(fig)
+
+            
+#def display_image_paired_dots(size,n1,n2,radius,dist,legend) : 
+#
+#    fig, ax = plt.subplots()
+#    dist_min = dist   
+#    list_dots = []   
+#    nbr_pairs = min(n1,n2)
+#    for pair in range(nbr_pairs) :
+#        check = 0
+#        while check < dist_min : 
+#            x1 ,y1 = rd.uniform(-size+dist,size-dist) , rd.uniform(-size+dist,size-dist) 
+#            check = check_distance_dots(x1,y1,list_dots,dist_min)
+#        list_dots.append((x1,y1))
+#        check = 0
+#        while check < dist_min : 
+#            dx , dy = rd.choice([rd.uniform(-300*radius,-dist*100), rd.uniform(300*radius,dist*100)])/100 , rd.choice([rd.uniform(-300*radius,-dist*50),rd.uniform(300*radius,dist*50)])/100
+#            x2 , y2 = x1 + dx , y1 + dy
+#            check = check_distance_dots(x2,y2,list_dots,dist_min)
+#        list_dots.append((x2,y2))
+#        ax.add_artist(plt.Circle((x1,y1),radius,color='b'))
+#        ax.add_artist(plt.Circle((x2,y2),radius,color='y'))
+#    nbr_isolated_dots = abs(n1-n2)
+#    color_dots = 'b'
+#    if n1 > n2 : 
+#        color_dots = 'y'
+#    for dot in range(nbr_isolated_dots) : 
+#        check = 0
+#        while check < dist_min : 
+#            x ,y = rd.uniform(-size+dist,size-dist) , rd.uniform(-size+dist,size-dist)
+#            check = check_distance_dots(x,y,list_dots,dist_min)
+#        ax.add_artist(plt.Circle((x,y),radius,color=color_dots))
+#    return mpld3.fig_to_html(fig)
+#            
+#    
+#    plt.xlabel(legend)
+#    ax.set_xlim((-size, size))
+#    ax.set_ylim((-size, size))
+#    plt.ion()
+##    plt.show()
+##    plt.pause(.001)
+#    return fig 
 
  
 
@@ -193,34 +162,34 @@ def save_result_to_csv(path,name,age,gender, colour_bld,eyesight,native_language
             spamwriter.writerow( (name, age,gender, colour_bld,eyesight,native_language,nbr_blue,nbr_red,ratio,answer,time,question) )
 
 
-def experiment_mth(exp) :
+#def experiment_mth(exp) :
+#    
+#    nbr_blue = exp[0][1]
+#    nbr_red = exp[0][0]
+#    ratio = float("{0:.2f}".format(nbr_blue/nbr_red))
+#    if exp[1] == 1 : 
+#        nbr_red,nbr_blue = nbr_blue,nbr_red
+##    if nbr_blue > nbr_red :
+##        correct_answer = 'Yes'
+##    else :
+##        correct_answer = 'No'
+#    list_blue, list_yellow = get_dots_coordinates(nbr_blue,nbr_yellow,dist)    
+##    answer,answer_time = record_RT()
+##    if answer == correct_answer : 
+##        answer = 'right'
+##    else : 
+##        answer = 'wrong'
+##    save_result_to_csv(save_path_csv,name,age,gender, colour_bld,eyesight,native_language,nbr_blue,nbr_red,ratio,answer,answer_time,'more than half')
+##    plt.close() 
+##    return plot
+#    return list_blue, list_yellow
     
-    nbr_blue = exp[0][1]
-    nbr_red = exp[0][0]
-    ratio = float("{0:.2f}".format(nbr_blue/nbr_red))
-    if exp[1] == 1 : 
-        nbr_red,nbr_blue = nbr_blue,nbr_red
-#    if nbr_blue > nbr_red :
-#        correct_answer = 'Yes'
-#    else :
-#        correct_answer = 'No'
-    list_blue, list_yellow = get_dots_coordinates(nbr_blue,nbr_yellow,dist)    
-#    answer,answer_time = record_RT()
-#    if answer == correct_answer : 
-#        answer = 'right'
-#    else : 
-#        answer = 'wrong'
-#    save_result_to_csv(save_path_csv,name,age,gender, colour_bld,eyesight,native_language,nbr_blue,nbr_red,ratio,answer,answer_time,'more than half')
-#    plt.close() 
-#    return plot
-    return list_blue, list_yellow
-    
-def main(exp) :
+def main() :
 
-    nbr_blue = exp[0][1]
-    nbr_yellow = exp[0][0]
+    nbr_blue = sys.argv[0][1]
+    nbr_yellow = sys.argv[0][0]
 #    ratio = float("{0:.2f}".format(nbr_blue/nbr_yellow))
-    if exp[1] == 1 : 
+    if sys.argv[1] == 1 : 
         nbr_red,nbr_blue = nbr_blue,nbr_yellow
 #    if nbr_blue > nbr_red :
 #        correct_answer = 'Yes'
@@ -238,18 +207,10 @@ def main(exp) :
 #    return list_blue, list_yellow
     print list_blue, list_yellow
         
-
-#    exp = experiments
-#    ind = index
-#    while ind != [] :
-#        print ind
-#        num = rd.choice(ind)       
-#        experiment_most(name,age,gender,colour_bld,eyesight,native_language,exp[num])
-#        ind.remove(num)
         
         
 #----- Call the main function to run the code --------------
     
-main(exp)
+main()
 
            
